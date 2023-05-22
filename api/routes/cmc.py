860f4cd0547d5843cbedfbd7999c787bf30c2f5f
Jeison Galeano <46209053+jeisonGaleano@users.noreply.github.com>
2023-05-22 08:52:43 -0500

    Json Galeano
    Despliegue Api CMC

--- api/routes/cmc.py
+++ api/routes/cmc.py
@@ -1,9 +1,15 @@
 from flask import Blueprint, request, jsonify, current_app
 
+from api.models.entities.Cmc import Cmc
+from api.models.CmcModel import CmcModel
 
 cmc = Blueprint('cmc', __name__)
 
 @cmc.route('/api/v1/cmc', methods=['GET'])
 def get_all():
 
-    return jsonify({'message': 'successfully'})
+    try:            
+        chevrolet = CmcModel.get_data_cmc()
+        return jsonify(chevrolet)
+    except Exception as ex:
+        return jsonify({'message': str(ex)}), 500  
